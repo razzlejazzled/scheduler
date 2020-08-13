d65ec77ecf247e01a7bf9e5c2fca1844bddea98a
RazzleJazzled <robertjgassner@gmail.com>
2020-08-12 17:26:51 -0700

    added changes

--- Develop/script.js
+++ Develop/script.js
@@ -1,3 +1,15 @@
-var currentDay = document.querySelector("#currentDay");
-var d = new Date();
-document.getElementById("currentDay").innerHTML = d;
+const save = document.querySelector(".save")
+const input = document.querySelector(".input")
+
+setInterval(function () {
+    $("#currentDay").text(moment().format("dddd, MMMM do YYYY, h:mm:ss a"));
+}, 1000);
+
+// WHEN I view the timeblocks for that day
+// THEN each timeblock is color coded to indicate whether it is in the past, present, or future
+// WHEN I click into a timeblock
+// THEN I can enter an event
+// WHEN I click the save button for that timeblock
+// THEN the text for that event is saved in local storage
+// WHEN I refresh the page
+// THEN the saved events persist
